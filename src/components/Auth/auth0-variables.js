c247407bbebb13dbd398f743f41d8730aeb2161e
srini <shriniva@gmail.com>
2020-06-13 19:05:00 -0400

    changing domain name to klasefi (classify) which is more apt

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'lbl-reviews.auth0.com',
   clientId: '4shpsEZ2RmOPLk63Ske2uJIK7JjN5VZ1',
-  callbackUrl: 'https://notchup.netlify.app/callback'
+  callbackUrl: 'https://klasefi.netlify.app/callback'
   //callbackUrl: 'http://localhost:3000/callback'
 };
