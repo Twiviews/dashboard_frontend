2813bcce70830b2d07f987c39482b9bd72802af2
srini <shriniva@gmail.com>
2020-06-14 17:01:02 -0400

    made label me button state to persist to db

--- src/components/Reviews/WorkedByComponent.js
+++ src/components/Reviews/WorkedByComponent.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { useMutation } from "@apollo/react-hooks";
+import { useQuery, useMutation, useEffect } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import Button from '@material-ui/core/Button';
 
 const ADD_LABELER = gql`
-mutation InsertLabeler($paragraph_id: Int) {
-  insert_labeler(objects: {paragraph_id: $paragraph_id}) {
+mutation InsertLabeler($paragraph_id: Int, $label_me: Boolean) {
+  insert_labeler(objects: {paragraph_id: $paragraph_id, label_me: $label_me}) {
     returning {
       labeler_user {
         username
@@ -14,13 +14,33 @@ mutation InsertLabeler($paragraph_id: Int) {
 }
 }
 `;
+const GET_LABELME = gql`
+query getLabelMe($paragraph_id: Int!) {
+  labeler_by_pk(paragraph_id: $paragraph_id) {
+    label_me
+  }
+}`;
 
 
+export default function WorkedByComponent({classes, paragraph_id}) {
 
-const WorkedByComponent = ({classes, paragraph_id}) => {
-  const [disabled, setDisabled] = useState(false); 
   const [addLabeler] = useMutation(ADD_LABELER);
+  const [labelMe, setLabelMe] = useState(false);
+
+  let label_me = false
+
+  const { loading, error, data } = useQuery(GET_LABELME, {variables: { paragraph_id: paragraph_id}});
+  if (loading) return <p>Loading ...</p>;
+  if (error) {
+    console.error(error);
+    return <div>Error!</div>;
+  }
+
+  if(data.labeler_by_pk !== null){
+    label_me = data.labeler_by_pk.label_me; 
+  }
   
+
 
   return (
   
@@ -28,16 +48,16 @@ const WorkedByComponent = ({classes, paragraph_id}) => {
         variant="contained"
         size="medium"
         color="inherit"
-        disabled = {disabled}
+        disabled = { label_me || labelMe } 
         onClick={e => {
           e.preventDefault();
-          addLabeler({ variables: { paragraph_id: paragraph_id}});
-          setDisabled(true)
+          addLabeler({ variables: { paragraph_id: paragraph_id, label_me: true}});
+          setLabelMe(true)
         }} >      
         Label Me!
     </Button> 
 
   );
-};
+}
 
-export default WorkedByComponent;
+
